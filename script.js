7fe4c55676221603ba9a5e6c4f620ed72f1482f6
Ethan Anderton <seven.swords.song@gmail.com>
2025-01-29 23:19:11 -0700

    old version

--- script.js
+++ script.js
@@ -2034,7 +2034,7 @@ function completedStep(div, list1, list2) {
     //--------------------------------------//
 
     //retrieve main doc div
-    document.getElementById('main').appendChild(div);
+    let mainDiv = document.getElementById('main').appendChild(div);
 
     //create container for next section 
     let container = document.createElement('div');
@@ -2055,12 +2055,12 @@ function completedStep(div, list1, list2) {
         container.appendChild(space);
         container.appendChild(nextDivs[nextIndex + 2]);
 
-        document.getElementById('main').removeChild(document.getElementById('score'));
-        alert('Child removed!');
+        // document.getElementById('main').removeChild(document.getElementById('score'));
+        // alert('Child removed!');
      
-        document.getElementById('main').appendChild(container);
+        mainDiv.appendChild(container);
 
-        document.getElementById('main').appendChild(document.getElementById('score')); 
+        // document.getElementById('main').appendChild(document.getElementById('score')); 
     }
 
     //reset flags to unselected 
